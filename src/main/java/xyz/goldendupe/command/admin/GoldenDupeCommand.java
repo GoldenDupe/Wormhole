8e1026099f7855804eca6ed87974af28f1ddc129
Antritus <antt.graft@gmail.com>
2024-03-05 15:56:33 +0200

    Reload command

--- src/main/java/xyz/goldendupe/command/admin/GoldenDupeCommand.java
+++ src/main/java/xyz/goldendupe/command/admin/GoldenDupeCommand.java
@@ -1,4 +1,67 @@
 package xyz.goldendupe.command.admin;
 
-public class GoldenDupeCommand {
+import bet.astral.cloudplusplus.annotations.Cloud;
+import bet.astral.messenger.Message;
+import bet.astral.messenger.Messenger;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.incendo.cloud.Command;
+import org.incendo.cloud.description.Description;
+import org.incendo.cloud.paper.PaperCommandManager;
+import xyz.goldendupe.GoldenDupe;
+import xyz.goldendupe.command.cloud.GDCloudCommand;
+import xyz.goldendupe.utils.MemberType;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+@Cloud
+public class GoldenDupeCommand extends GDCloudCommand {
+	public GoldenDupeCommand(GoldenDupe plugin, PaperCommandManager<CommandSender> commandManager) {
+		super(plugin, commandManager);
+		Command.Builder<CommandSender> builder = commandBuilder("goldendupe",
+				Description.of("GoldenDupe is known for it's shitty community.")
+				)
+				.handler(context->{
+					context.sender().sendMessage("Hello goldenwupe!");
+				});
+		command(builder);
+		command(builder.literal("reload")
+				.permission(MemberType.OWNER.permissionOf("goldendupe.reload"))
+				.handler(context->{
+					for (int i = 0; i < 3; i++){
+						Bukkit.broadcast(MiniMessage.miniMessage().deserialize(
+								"<gold><bold>Golden<white>Dupe<reset> <red>GoldenDupe will be reloading! Please wait as it might lag for a bit!"
+						));
+					}
+					plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin,
+							()->{
+								try {
+									Field field = Messenger.class.getDeclaredField("messagesMap");
+									field.setAccessible(true);
+									Map<String, Message> messageMap = (Map<String, Message>) field.get(plugin.commandMessenger());
+
+									plugin.reloadConfig();
+									plugin.reloadMessengers();
+									plugin.getGlobalData().reload();
+
+									for (String key : messageMap.keySet()){
+										plugin.messenger().loadMessage(key);
+									}
+								} catch (Exception e) {
+									Bukkit.broadcast(MiniMessage.miniMessage().deserialize(
+											"<gold><bold>Golden<white>Dupe<reset> <red>GoldenDupe failed to reload the server! Check console for the error! A server restart is required!"
+									));
+									e.printStackTrace();
+									return;
+								}
+
+								Bukkit.broadcast(MiniMessage.miniMessage().deserialize(
+										"<gold><bold>Golden<white>Dupe<reset> <green>GoldenDupe has reloaded successfully!"
+								));
+							}, 20);
+				})
+		);
+	}
 }
