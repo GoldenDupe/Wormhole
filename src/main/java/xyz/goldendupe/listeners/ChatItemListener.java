0c04d0088cc05e872989ea5986fa3639f3e0df40
Antritus <antt.graft@gmail.com>
2024-04-06 09:49:51 +0300

    Try to patch item name serializer not working correctly

--- src/main/java/xyz/goldendupe/listeners/ChatItemListener.java
+++ src/main/java/xyz/goldendupe/listeners/ChatItemListener.java
@@ -11,9 +11,13 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import xyz.goldendupe.GoldenDupe;
 import xyz.goldendupe.models.chatcolor.Color;
+import xyz.goldendupe.utils.MemberType;
+
+import java.util.regex.Pattern;
 
 
 public class ChatItemListener implements GDListener {
+	private final Pattern pattern = Pattern.compile("((?i)\\[item]|\\[i])");
 	private final GoldenDupe goldenDupe;
 
 	public ChatItemListener(GoldenDupe goldenDupe) {
@@ -24,25 +28,28 @@ public class ChatItemListener implements GDListener {
 	private void chatItem(AsyncChatDecorateEvent event){
 		Player player = event.player();
 		assert player != null;
+		if (!player.hasPermission(MemberType.DONATOR.permissionOf("chat.item"))){
+			return;
+		}
 
 		String plain = PlainTextComponentSerializer.plainText().serialize(event.result());
 		if (plain.toLowerCase().contains("[item]") || plain.toLowerCase().contains("[i]")){
 			ItemStack itemStack = player.getInventory().getItemInMainHand();
-			Component itemComponent = Component.text("");
+			Component itemComponent;
 			if (itemStack.isEmpty()){
 				itemComponent = Component.text("1x", Color.GREEN).appendSpace().append(Component.text(player.getName(), Color.MINECOIN)).append(Component.text("'s hand", Color.MINECOIN)).hoverEvent(HoverEvent.showText(Component.text("Nothing to see here, it's just a hand.", Color.WHITE)));
 			} else {
 				ItemMeta meta = itemStack.getItemMeta();
+				Component displayname = meta.hasDisplayName() ? meta.displayName() : Component.translatable(itemStack.translationKey());
+
 				itemComponent = Component.text(itemStack.getAmount()+"x", NamedTextColor.GREEN)
-						.appendSpace().append(meta.hasDisplayName() ? meta.displayName() : Component.translatable(itemStack.translationKey()))
+						.appendSpace().append(displayname)
 						.hoverEvent(itemStack)
 				;
 			}
 			Component finalItemComponent = itemComponent;
 			event.result(event.result().replaceText(builder->
-							builder.match("(?i)[item]").replacement(finalItemComponent))
-					.replaceText(builder -> builder.match("(?i)[i]").replacement(finalItemComponent))
-			);
+							builder.match(pattern).replacement(finalItemComponent)));
 		}
 	}
 
