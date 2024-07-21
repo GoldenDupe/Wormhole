0b04967f1bc6446b7e02f29ad70499fd3f2ff434
Antritus <antt.graft@gmail.com>
2024-07-21 21:19:28 +0300

    Replace message manager

--- src/main/java/xyz/goldendupe/listeners/IllegalListener.java
+++ src/main/java/xyz/goldendupe/listeners/IllegalListener.java
@@ -1,11 +1,12 @@
 package xyz.goldendupe.listeners;
 
-import bet.astral.messenger.placeholder.Placeholder;
+import bet.astral.messenger.v2.placeholder.Placeholder;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockPlaceEvent;
 import xyz.goldendupe.GoldenDupe;
+import xyz.goldendupe.messenger.Translations;
 import xyz.goldendupe.utils.MemberType;
 import xyz.goldendupe.utils.StringUtils;
 
@@ -25,7 +26,7 @@ public class IllegalListener implements GDListener {
 			}
 			event.setCancelled(true);
 			event.setBuild(false);
-			goldenDupe.messenger().message(event.getPlayer(), "cannot-place-illegal", new Placeholder("block", StringUtils.properCase(block.getType().name())));
+			goldenDupe.messenger().message(event.getPlayer(), Translations.LISTENER_ILLEGAL, Placeholder.of("block", StringUtils.properCase(block.getType().name())));
 		}
 	}
 
