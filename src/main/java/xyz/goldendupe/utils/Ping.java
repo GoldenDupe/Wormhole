fefa1c95f4f866726d2285c1aaef20757750884e
Antritus <antt.graft@gmail.com>
2024-07-21 21:19:13 +0300

    Convert to bet.astral.tuples.Triplet

--- src/main/java/xyz/goldendupe/utils/Ping.java
+++ src/main/java/xyz/goldendupe/utils/Ping.java
@@ -1,32 +1,33 @@
 package xyz.goldendupe.utils;
 
+import bet.astral.tuples.Triplet;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 
 public class Ping {
 	public static Ping defaultPing = new Ping(
 			Component.text("Loading...", NamedTextColor.RED),
-			Triple.of(0, 75, NamedTextColor.DARK_GREEN),
-			Triple.of(76, 110, NamedTextColor.GREEN),
-			Triple.of(111, 160, NamedTextColor.YELLOW),
-			Triple.of(161, 220, NamedTextColor.RED),
+			Triplet.immutable(0, 75, NamedTextColor.DARK_GREEN),
+			Triplet.immutable(76, 110, NamedTextColor.GREEN),
+			Triplet.immutable(111, 160, NamedTextColor.YELLOW),
+			Triplet.immutable(161, 220, NamedTextColor.RED),
 			NamedTextColor.DARK_RED,
 			true
 			);
 	private final Component loading;
-	private final Triple<Integer, Integer, NamedTextColor> bestValue;
-	private final Triple<Integer, Integer, NamedTextColor> goodValue;
-	private final Triple<Integer, Integer, NamedTextColor> okValue;
-	private final Triple<Integer, Integer, NamedTextColor> worseValue;
+	private final Triplet<Integer, Integer, NamedTextColor> bestValue;
+	private final Triplet<Integer, Integer, NamedTextColor> goodValue;
+	private final Triplet<Integer, Integer, NamedTextColor> okValue;
+	private final Triplet<Integer, Integer, NamedTextColor> worseValue;
 	private final NamedTextColor worstValue;
 	private final boolean showMS;
 
 	public Ping(
 			Component loading,
-			Triple<Integer, Integer, NamedTextColor> bestValue,
-			Triple<Integer, Integer, NamedTextColor> goodValue,
-			Triple<Integer, Integer, NamedTextColor> okValue,
-			Triple<Integer, Integer, NamedTextColor> worseValue,
+			Triplet<Integer, Integer, NamedTextColor> bestValue,
+			Triplet<Integer, Integer, NamedTextColor> goodValue,
+			Triplet<Integer, Integer, NamedTextColor> okValue,
+			Triplet<Integer, Integer, NamedTextColor> worseValue,
 			NamedTextColor worstValue,
 			boolean showMS
 			) {
@@ -40,19 +41,19 @@ public class Ping {
 	}
 
 
-	public Triple<Integer, Integer, NamedTextColor> bestValue() {
+	public Triplet<Integer, Integer, NamedTextColor> bestValue() {
 		return bestValue;
 	}
 
-	public Triple<Integer, Integer, NamedTextColor> goodValue() {
+	public Triplet<Integer, Integer, NamedTextColor> goodValue() {
 		return goodValue;
 	}
 
-	public Triple<Integer, Integer, NamedTextColor> okValue() {
+	public Triplet<Integer, Integer, NamedTextColor> okValue() {
 		return okValue;
 	}
 
-	public Triple<Integer, Integer, NamedTextColor> worseValue() {
+	public Triplet<Integer, Integer, NamedTextColor> worseValue() {
 		return worseValue;
 	}
 
@@ -69,14 +70,14 @@ public class Ping {
 			return Component.text("Loading...", NamedTextColor.RED);
 		}
 		NamedTextColor color = NamedTextColor.DARK_RED;
-		if (ping>=bestValue.first && ping <= bestValue.second) {
-			color = bestValue.third();
-		} else if (ping>=goodValue.first && ping <= goodValue.second) {
-			color = goodValue.third();
-		} else if (ping>=okValue.first && ping <= okValue.second) {
-			color = okValue.third();
-		} else if (ping>=worseValue.first && ping <= worseValue.second) {
-			color = worseValue.third();
+		if (ping>=bestValue.getFirst() && ping <= bestValue.getSecond()) {
+			color = bestValue.getThird();
+		} else if (ping>=goodValue.getFirst() && ping <= goodValue.getSecond()) {
+			color = goodValue.getThird();
+		} else if (ping>=okValue.getFirst() && ping <= okValue.getSecond()) {
+			color = okValue.getThird();
+		} else if (ping>=worseValue.getFirst() && ping <= worseValue.getSecond()) {
+			color = worseValue.getThird();
 		} else {
 			color = worstValue;
 		}
