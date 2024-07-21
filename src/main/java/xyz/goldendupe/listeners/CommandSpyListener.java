42163e8db539ec597a2ee18e2e17476c5970b1e5
Antritus <antt.graft@gmail.com>
2024-07-21 21:20:11 +0300

    Command manager update

--- src/main/java/xyz/goldendupe/listeners/CommandSpyListener.java
+++ src/main/java/xyz/goldendupe/listeners/CommandSpyListener.java
@@ -1,14 +1,17 @@
 package xyz.goldendupe.listeners;
 
-import bet.astral.messenger.placeholder.Placeholder;
+import bet.astral.messenger.v2.paper.receiver.PlayerReceiver;
+import bet.astral.messenger.v2.permission.PredicatePermission;
+import bet.astral.messenger.v2.placeholder.Placeholder;
 import io.papermc.paper.event.player.PlayerSignCommandPreprocessEvent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.incendo.cloud.permission.PredicatePermission;
 import xyz.goldendupe.GoldenDupe;
+import xyz.goldendupe.messenger.Translations;
 import xyz.goldendupe.models.astronauts.CSPYUser;
 
 import java.util.Set;
@@ -24,11 +27,11 @@ public class CommandSpyListener implements GDListener {
 	public void onCommandPreprocess(PlayerCommandPreprocessEvent event){
 		Player whoExecuted = event.getPlayer();
 		String command = event.getMessage();
-		goldenDupe.commandMessenger()
+		goldenDupe.messenger()
 				.broadcast(
 						PredicatePermission.of(sender -> {
-							if (sender instanceof Player player) {
-								CSPYUser user = goldenDupe.commandSpyDatabase().fromPlayer(player);
+							if (sender instanceof PlayerReceiver player) {
+								CSPYUser user = goldenDupe.commandSpyDatabase().fromPlayer(player.getPlayer());
 								if (user.isCommandSpyToggled()) {
 									if (user.blockedUsers().contains(whoExecuted.getUniqueId())) {
 										return false;
@@ -44,20 +47,20 @@ public class CommandSpyListener implements GDListener {
 							}
 							return false;
 						}),
-						"commandspy.message-executed", new Placeholder("player", whoExecuted.getName()), new Placeholder("command", command),
-						new Placeholder("command_copy", Component.text(command).clickEvent(ClickEvent.copyToClipboard(command))),
-						new Placeholder("command_suggest", Component.text(command).clickEvent(ClickEvent.suggestCommand(command)))
+						Translations.LISTENER_COMMAND_SPY_EXECUTED, Placeholder.of("player", whoExecuted.getName()), Placeholder.of("command", command),
+						Placeholder.of("command_copy", Component.text(command).clickEvent(ClickEvent.copyToClipboard(command)).hoverEvent(HoverEvent.showText(Component.text("Click to copy this command to your clipboard.")))),
+						Placeholder.of("command_suggest", Component.text(command).clickEvent(ClickEvent.suggestCommand(command)).hoverEvent(HoverEvent.showText(Component.text("Click to suggest this command to your chat box."))))
 				);
 	}
 	@EventHandler
 	public void onCommandSignPreprocess(PlayerSignCommandPreprocessEvent event){
 		Player whoExecuted = event.getPlayer();
 		String command = event.getMessage();
-		goldenDupe.commandMessenger()
+		goldenDupe.messenger()
 				.broadcast(
 						PredicatePermission.of(sender -> {
-							if (sender instanceof Player player) {
-								CSPYUser user = goldenDupe.commandSpyDatabase().fromPlayer(player);
+							if (sender instanceof PlayerReceiver player) {
+								CSPYUser user = goldenDupe.commandSpyDatabase().fromPlayer(player.getPlayer());
 								if (user.isCommandSpyToggled()) {
 									if (user.blockedUsers().contains(whoExecuted.getUniqueId())) {
 										return false;
@@ -72,9 +75,14 @@ public class CommandSpyListener implements GDListener {
 							}
 							return false;
 						}),
-						"commandspy.message-executed-sign", new Placeholder("player", whoExecuted.getName()), new Placeholder("command", command),
-						new Placeholder("command_copy", Component.text(command).clickEvent(ClickEvent.copyToClipboard(command))),
-						new Placeholder("command_suggest", Component.text(command).clickEvent(ClickEvent.suggestCommand(command)))
+						Translations.LISTENER_COMMAND_SPY_EXECUTED_SIGN,
+						Placeholder.of("player", whoExecuted.getName()), Placeholder.of("command", command),
+						Placeholder.of("command_copy", Component.text(command).clickEvent(ClickEvent.copyToClipboard(command)).hoverEvent(HoverEvent.showText(Component.text("Click to copy this command to your clipboard.")))),
+						Placeholder.of("command_suggest", Component.text(command).clickEvent(ClickEvent.suggestCommand(command)).hoverEvent(HoverEvent.showText(Component.text("Click to suggest this command to your chat box.")))),
+						Placeholder.of("x", (int) event.getSign().getLocation().getX()),
+						Placeholder.of("y", (int) event.getSign().getLocation().getY()),
+						Placeholder.of("z", (int) event.getSign().getLocation().getZ()),
+						Placeholder.of("world", event.getSign().getLocation().getWorld().getName())
 				);
 	}
 
