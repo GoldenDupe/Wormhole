a463928db1037d067b68ee8594c23b69a5f857e6
Antritus <antt.graft@gmail.com>
2024-11-05 16:07:18 +0200

    Patch outdated guiman code to latest

--- src/main/java/xyz/goldendupe/command/donator/og/CreativeGUICommand.java
+++ src/main/java/xyz/goldendupe/command/donator/og/CreativeGUICommand.java
@@ -1,17 +1,19 @@
 package xyz.goldendupe.command.donator.og;
 
 import bet.astral.cloudplusplus.annotations.Cloud;
-import bet.astral.guiman.ClickableBuilder;
-import bet.astral.guiman.GUI;
-import bet.astral.guiman.GUIBuilder;
+import bet.astral.guiman.background.Background;
+import bet.astral.guiman.clickable.Clickable;
+import bet.astral.guiman.gui.InventoryGUI;
+import bet.astral.guiman.gui.builders.InventoryGUIBuilder;
+import bet.astral.guiman.utils.ChestRows;
 import bet.astral.messenger.v2.component.ComponentType;
 import bet.astral.messenger.v2.placeholder.Placeholder;
-import com.sun.jna.platform.win32.AccCtrl;
+import bet.astral.messenger.v2.placeholder.collection.PlaceholderCollection;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Registry;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.ItemStack;
 import org.incendo.cloud.description.Description;
@@ -29,51 +31,54 @@ import java.util.*;
 
 @Cloud
 public class CreativeGUICommand extends GDCloudCommand implements InitAfterBootstrap {
-	private final Map<Integer, GUI> guis = new IdentityHashMap<>();
+	private final Map<Integer, InventoryGUI> guis = new IdentityHashMap<>(200);
 
 	public CreativeGUICommand(GoldenDupeCommandRegister register, PaperCommandManager.Bootstrapped<CommandSender> commandManager) {
 		super(register, commandManager);
 		commandPlayer(commandBuilderPlayer("blocks", Description.of("Allows player to take items from a menu with all blocks in the server. Illegal items will not be in the menu."), "blocksmenu", "blockmenu")
 				.permission(MemberType.DONATOR.permissionOf("blocks-menu"))
 				.handler(context -> {
-					guis.get(0).generateInventory(context.sender());
+					guis.get(0).open(context.sender());
 				})
 		);
 	}
 
 
-	private GUI create(@NotNull List<Material> materials, int page, int maxPages, boolean isLastPage, int from, int to) {
-		GUIBuilder builder = new GUIBuilder(6);
-		builder.setBackground(new ClickableBuilder(Material.BLACK_STAINED_GLASS_PANE, meta -> meta.setHideTooltip(true)));
+	private InventoryGUI create(@NotNull List<Material> materials, int page, int maxPages, boolean isLastPage, int from, int to) {
+		InventoryGUIBuilder builder = InventoryGUI.builder(ChestRows.SIX);
+        builder.background(Background.noTooltip(Material.BLACK_STAINED_GLASS_PANE));
 		List<Material> copy = materials.subList(from, Math.min(to, materials.size()));
 		int slot = 0;
 		for (Material material : copy) {
-			builder.addSlotClickable(slot, new ClickableBuilder(material, meta -> {
+			builder.addClickable(slot, Clickable.builder(material, meta -> {
 				meta.displayName(getComponent(Translations.GUI_CREATIVE_ITEM_NAME, Placeholder.of("block", Component.translatable(material.translationKey()))));
 				meta.lore(List.of());
-			}).setGeneralAction((clickable, item, player) -> {
-				player.getInventory().addItem(ItemStack.of(item.getType(), 1));
-			}));
+			}).action(List.of(ClickType.LEFT, ClickType.RIGHT), (action) -> {
+				action.getWho().getInventory().addItem(ItemStack.of(action.getItemStack().getType(), 1));
+			}).action(List.of(ClickType.SHIFT_LEFT, ClickType.SHIFT_RIGHT), (action)-> {
+				action.getWho().getInventory().addItem(ItemStack.of(action.getItemStack().getType(),
+						action.getItemStack().getType().getMaxStackSize()));
+			})
+			);
 			slot++;
 		}
-		builder.setBackground(new ClickableBuilder(Material.BLACK_STAINED_GLASS_PANE, meta -> meta.setHideTooltip(true)));
 		if (page > 0) {
-			builder.setSlotClickable(45, new ClickableBuilder(Material.ARROW, meta -> {
+			builder.addClickable(45, Clickable.builder(Material.ARROW, meta -> {
 				meta.displayName(getComponent(Translations.GUI_CREATIVE_BACK));
-			}).setGeneralAction((clickable, itemStack, player) -> guis.get(page - 1).generateInventory(player)));
+			}).actionGeneral((action) -> guis.get(page - 1).open(action.getWho())));
 		}
 		if (!isLastPage) {
-			builder.setSlotClickable(53, new ClickableBuilder(Material.ARROW, meta -> {
+			builder.addClickable(53, Clickable.builder(Material.ARROW, meta -> {
 				meta.displayName(getComponent(Translations.GUI_CREATIVE_NEXT));
-			}).setGeneralAction((clickable, itemStack, player) -> guis.get(page + 1).generateInventory(player)));
+			}).actionGeneral((action) -> guis.get(page + 1).open(action.getWho())));
 		}
-		builder.setSlotClickable(49, new ClickableBuilder(Material.BARRIER, meta -> {
+		builder.addClickable(49, Clickable.builder(Material.BARRIER, meta -> {
 			meta.displayName(getComponent(Translations.GUI_CREATIVE_CLOSE));
-		}).setGeneralAction((clickable, item, player) -> player.closeInventory(InventoryCloseEvent.Reason.PLAYER)));
-		builder.name(getComponent(Translations.GUI_CREATIVE_TITLE, Placeholder.of("page", page+1), Placeholder.of("max_page", maxPages)));
-		GUI gui = builder.build();
-		gui.shared = false;
-		return gui;
+		}).actionGeneral((action) ->
+				action.getWho().closeInventory(InventoryCloseEvent.Reason.PLAYER)));
+		builder.title(Translations.GUI_CREATIVE_TITLE);
+		builder.placeholderGenerator(_ -> PlaceholderCollection.list(Placeholder.of("page", page+1), Placeholder.of("max_page", maxPages)));
+        return builder.build();
 	}
 
 	public Component getComponent(Translations.Translation translation, Placeholder... placeholders) {
@@ -92,7 +97,7 @@ public class CreativeGUICommand extends GDCloudCommand implements InitAfterBoots
 		int from = 0;
 		int to = add;
 		for (int i = 0; i < pages; i++) {
-			GUI gui = create(materials, i, pages, i+1==pages, from, to);
+			InventoryGUI gui = create(materials, i, pages, i+1==pages, from, to);
 			guis.put(i, gui);
 			from+=add;
 			to+=add;
